36d0e6b9562714fe2ae26d885598c86df0d7cac2
anyepeter <petzyrockchendi@gmail.com>
2023-04-05 22:32:41 +0100

    install linters

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,6 @@
 class ApiController < ApplicationController
-    def greetings
-      @messages = Message.order('RANDOM()').first
-      render json: @messages
-    end
+  def greetings
+    @messages = Message.order('RANDOM()').first
+    render json: @messages
   end
+end
