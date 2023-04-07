48d3c75342f199220789779db7d7fc7474ac158c
anyepeter <petzyrockchendi@gmail.com>
2023-04-07 14:45:28 +0100

    Add data for the seed test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+greetings1 = Message.create(message: 'Good morning')
+greetings2 = Message.create(message: 'Good evening')
+greetings3 = Message.create(message: 'Good afternoon')
+greetings4 = Message.create(message: 'Good bye')
+greetings5 = Message.create(message: 'See you soon')
